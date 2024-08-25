089559e4092fe45a3713159bccc709fb310b3872
nvpetrov93@gmail.com <nvpetrov93@gmail.com>
2024-08-25 10:10:00 +0300

    End of Section 5

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal? Price) : ICommand<CreateProductResult>;
+public record CreateProductResult (Guid Id);
+
+internal class CreateProductCommandHandler(IDocumentSession documentSession)
+    : ICommandHandler<CreateProductCommand,CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Categories = command.Categories,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+        }; 
+
+        documentSession.Store(product);
+        documentSession.SaveChanges();
+
+        return new CreateProductResult(product.Id);
+    }
+}
